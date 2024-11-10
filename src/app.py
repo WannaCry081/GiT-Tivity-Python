4d078d75e32ccc6904269d5f30abd89ba98b6cf3
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-11-10 16:31:02 +0800

    chore: add 'commit_changes static method

--- src/app.py
+++ src/app.py
@@ -1,2 +1,9 @@
+from src.utils import CommandUtil
+
+
 class Program:
-    pass
+    
+    @staticmethod
+    def commit_changes(commit_message: str) -> None:
+        CommandUtil.execute_with_result(cmd="git add .")
+        CommandUtil.execute_with_result(cmd_lst=["git", "commit", "-m", commit_message])
