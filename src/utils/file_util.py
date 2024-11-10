5112f236741b9ee5a1c94263758ae1e1ec430921
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-11-10 15:48:35 +0800

    chore: add 'create_analytics_file' static method

--- src/utils/file_util.py
+++ src/utils/file_util.py
@@ -8,3 +8,21 @@ class FileUtil:
     def open_json_file(file_name: str) -> Optional[dict]:
         with open(file_name, "r") as file:
             return json.load(file)
+
+    @staticmethod
+    def create_analytics_file(file_name: str, **kwargs) -> None:
+        resource = {
+            "git": {
+                "name": kwargs["git_name"],
+                "email": kwargs["git_email"],
+                "branch": kwargs["git_branch"],
+            },
+            "no_of_commits": 1,
+            "no_of_files": kwargs["no_of_files"],
+            "created_at": kwargs["current_day"],
+            "updated_at": "",
+        }
+
+        with open(file_name, "w") as file:
+            json.dump(resource, file, indent=4)
+
