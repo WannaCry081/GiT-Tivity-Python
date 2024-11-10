46d958e58e12097c51c8beff06575faf4b258065
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-11-10 16:00:27 +0800

    chore: add 'execute_with_result' static method

--- src/utils/command_util.py
+++ src/utils/command_util.py
@@ -1,2 +1,22 @@
+import subprocess
+
+
 class CommandUtil:
-    pass
+  
+    @staticmethod
+    def execute_with_result(
+        cmd: Optional[str] = None,
+        cmd_lst: Optional[List[str]] = None,
+    ) -> subprocess.CompletedProcess:
+      
+        assert (cmd is None) != (
+            cmd_lst is None
+        ), "Provide either 'cmd' or 'cmd_lst', not both."
+
+        command = cmd.split() if cmd else cmd_lst
+        try:
+            result = subprocess.run(command, capture_output=True, text=True, check=True)
+            return result
+        except subprocess.CalledProcessError as e:
+            print(f"Command execution failed: {e}")
+            return e
