a008f04f5ea36445d5cfa2eae56bc56855445e3b
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-11-11 15:45:08 +0800

    ref: remove unnecessary try-catch block

--- main.py
+++ main.py
@@ -4,13 +4,5 @@ from src import Program
 
 
 if __name__ == "__main__":
-    try:
-        exit_code = Program.main(sys.argv[1:])
-
-    except Exception as e:
-        logging.error(
-            "An unexpected error occurred while executing the program.", exc_info=True
-        )
-        exit_code = -1
-
+    exit_code = Program.main(sys.argv[1:])
     sys.exit(exit_code)
